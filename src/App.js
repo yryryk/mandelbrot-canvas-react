9e667d6cc35941a7dd3352d4a1e31143bc7f0713
yryryk <tovchennikov@e1.ru>
2023-06-25 20:45:32 +0500

    feat: добавить регулировку цвета

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { useState, useRef, useEffect } from 'react';
 
 function App() {
   const canvasRef = useRef(null);
+  // const width = 2000;
+  // const height = 2000;
   const width = window.innerWidth;
-  const height = window.innerHeight-5;
+  const height = window.innerHeight-29;
   const [centerX, setCenterX] = useState(-1);
   const [centerY, setCenterY] = useState(0);
   const [zoom, setZoom] = useState(1/4);
@@ -63,11 +65,6 @@ function App() {
     }
   }
 
-  const handleWheel = (evt) => {
-    const delta = evt.deltaY>0?-1:1;
-    changeZoom(delta, evt);
-  };
-
   return (
     <main className="App">
       <Mandelbrot
@@ -85,7 +82,6 @@ function App() {
         handleZoomIn={handleZoomIn}
         handleZoomOut={handleZoomOut}
         handleZoom={handleZoom}
-        handleWheel={handleWheel}
       />
     </main>
   );
@@ -97,6 +93,43 @@ export default App;
 // newCenterX: -1.250019962517241
 // centerY: -0.0020125489954753
 //maxIterations: 588133
+
 // newZoom: 8796093022208
-// Mandelbrot.jsx:78 newCenterX: -1.2501393169121626
-// Mandelbrot.jsx:79 centerY: -0.005371111353860215
+// newCenterX: -1.2501393169121626
+// centerY: -0.005371111353860215
+
+// newZoom: 2097152
+// newCenterX: -1.3681111931091305
+// newCenterY: 0.0007431717912730078
+// maxIterations: 37244
+
+// newZoom: 4194304
+// newCenterX: -1.368107891139992
+// newCenterY: 0.0006355606042302774
+// maxIterations: 42792
+
+// newZoom: 8388608
+// newCenterX: -1.3681058664246863
+// newCenterY: 0.0005586961046715917
+// maxIterations: 48868
+
+// newZoom: 67108864
+// App.js:43 newCenterX: 0.359935434651561
+// App.js:44 newCenterY: -0.6415037419414149
+// Mandelbrot.jsx:25 maxIterations: 70504
+
+// newZoom: 67108864
+// App.js:43 newCenterX: -1.25000022060405
+// App.js:44 newCenterY: 0.00037556747770590054
+// Mandelbrot.jsx:25 maxIterations: 70504
+
+// newZoom: 2147483648
+// App.js:43 newCenterX: -1.7864896635665224
+// App.js:44 newCenterY: 8.679844307530769e-8
+// Mandelbrot.jsx:25 maxIterations: 119364
+
+// newZoom: 134217728
+// App.js:43 newCenterX: -1.2568159078628365
+// App.js:44 newCenterY: 0.3797229409375518
+// Mandelbrot.jsx:27 maxIterations: 78932
+
--- src/Mandelbrot.jsx
+++ src/Mandelbrot.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './Mandelbrot.css';
 
 function Mandelbrot(props) {
@@ -18,15 +18,53 @@ function Mandelbrot(props) {
     handleZoomOut,
     handleZoom,
   } = props;
+  const [iterations, setIterations] = useState([]);
+  const [maxIterations, setMaxIterations] = useState(0);
+  const [red, setRed] = useState(37);
+  const [green, setGreen] = useState(23);
+  const [blue, setBlue] = useState(61);
+  const modifierR = useRef(null);
+  const modifierG = useRef(null);
+  const modifierB = useRef(null);
+
+  useEffect (() => {
+    function setRedValue(evt) {
+      setRed(evt.target.value)
+    };
+    function setGreenValue(evt) {
+      setGreen(evt.target.value)
+    };
+    function setBlueValue(evt) {
+      setBlue(evt.target.value)
+    };
+    let rModifier;
+    let gModifier;
+    let bModifier;
+    if(modifierR&&modifierG&&modifierB) {
+      rModifier = modifierR.current;
+      gModifier = modifierG.current;
+      bModifier = modifierB.current;
+
+      rModifier.addEventListener('change', setRedValue);
+      gModifier.addEventListener('change', setGreenValue);
+      bModifier.addEventListener('change', setBlueValue);
+    }
+    return () => {
+      rModifier.removeEventListener('change', setRedValue);
+      gModifier.removeEventListener('change', setGreenValue);
+      bModifier.removeEventListener('change', setBlueValue);
+
+    }
+  }, [])
 
   useEffect(() => {
-    if (ctx) {
-      const maxIterations = Math.floor(200 + maxIterationsCoefficient*4*Math.log2(zoom)**3);
-      console.log(`maxIterations: ${maxIterations}`);
+    const data = () => {
+      const maxIterationsInner = Math.floor(200 + maxIterationsCoefficient*4*Math.log2(zoom)**3);
+      console.log(`maxIterations: ${maxIterationsInner}`);
       console.log("");
-      let imageData = ctx.getImageData(0, 0, width, height);
-      let data = imageData.data;
+      const arr = [];
       for (let y = 0; y < height; y++) {
+        const subArr = [];
         for (let x = 0; x < width; x++) {
           const cx = centerX + (x - width / 2) / (width * zoom);
           const cy = centerY + (y - height / 2)  / (width * zoom);
@@ -37,12 +75,42 @@ function Mandelbrot(props) {
           let zy2 = 0;
           let i = 0;
 
-          while (zx2 + zy2 < 4 && i < maxIterations) {
+          while (zx2 + zy2 < 4 && i < maxIterationsInner) {
             zy = 2 * zx * zy + cy;
             zx = zx2 - zy2 + cx;
             zx2 = zx * zx;
             zy2 = zy * zy;
             i++;
+          }
+          subArr.push(i);
+        }
+        arr.push(subArr)
+      }
+      return {arr, maxIterationsInner}
+    }
+
+    async function calculate() {
+      const iterations = await data();
+      if(iterations) {
+        setIterations(iterations.arr)
+        setMaxIterations(iterations.maxIterationsInner);
+      }
+    };
+
+    if (ctx) {
+      calculate()
+    }
+  }, [width, height, maxIterationsCoefficient, ctx, centerX, centerY, zoom, maxIterations]);
+
+  useEffect(() => {
+    function renderCanvas(ctx, maxIterations, rgbModifiers) {
+      let imageData = ctx.getImageData(0, 0, width, height);
+      let data = imageData.data;
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          const i = iterations[y][x];
+          if (x === 1 && y === 1) {
+            console.log(i);
           }
           const index = (y * width + x) * 4;
           if (i === maxIterations) {
@@ -51,9 +119,9 @@ function Mandelbrot(props) {
             data[index+2] = 0;
             data[index+3] = 255;
           } else {
-            const r = 127 + 64 * Math.sin((1/50) * i);
-            const g = 127 + 64 * Math.sin((1/50) * i);
-            const b = 127 + 64 * Math.cos((1/40) * i);
+            const r = (127 + 64 * Math.sin((1/rgbModifiers.r) * i));
+            const g = (127 + 64 * Math.sin((1/rgbModifiers.g) * i));
+            const b = (127 + 64 * Math.sin((1/rgbModifiers.b) * i));
             const saturation = (color, n) => Math.floor(((color/255)**(n/(color + 1)))*255)
             const n = 128;
             data[index] = saturation(r, n);
@@ -65,14 +133,25 @@ function Mandelbrot(props) {
       }
       ctx.putImageData(imageData, 0, 0);
     }
-  }, [width, height, maxIterationsCoefficient, ctx, centerX, centerY, zoom]);
+    if (ctx&&maxIterations) {
+      renderCanvas(ctx, maxIterations, {r: red,g: green,b: blue})
+    }
+  }, [blue, ctx, green, height, iterations, maxIterations, red, width]);
 
   return (
     <div className="mandelbrot">
-      <canvas className='mandelbrot__canvas' ref={canvasRef} width={width} height={height} onWheel={handleWheel} onClick={handleZoom} />
-      <div className="mandelbrot__button-container">
+      <div className={`mandelbrot__button-container ${isZoomIn ? " mandelbrot__button-container_zoom-in" : ""} ${ isZoomOut ? " mandelbrot__button-container_zoom-out" : ""}`}>
         <button onClick={handleZoomIn} className={`mandelbrot__button mandelbrot__button_in${isZoomIn?" mandelbrot__button_active":""}`}></button>
         <button onClick={handleZoomOut} className={`mandelbrot__button mandelbrot__button_out${isZoomOut?" mandelbrot__button_active":""}`}></button>
+      </div>
+      <canvas className='mandelbrot__canvas' ref={canvasRef} width={width} height={height} onWheel={handleWheel} onClick={handleZoom} />
+      <div className="mandelbrot__modifiers">
+        <label htmlFor='r'>R-spread</label>
+        <input ref={modifierR} name='r' className="mandelbrot__modifiers-r" type='number' min={1} max={1000} defaultValue={red} />
+        <label htmlFor='r'>G-spread</label>
+        <input ref={modifierG} name='g' className="mandelbrot__modifiers-g" type='number' min={1} max={1000} defaultValue={green} />
+        <label htmlFor='r'>B-spread</label>
+        <input ref={modifierB} name='b' className="mandelbrot__modifiers-b" type='number' min={1} max={1000} defaultValue={blue} />
       </div>
     </div>
   );
